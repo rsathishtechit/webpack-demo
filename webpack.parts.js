78ccd031e7f3bde89174bb7fd2094918185145ad
Sathishkumar R <rsathishtechit@gmail.com>
2023-08-10 13:21:10 +0530

    Added css-loader and style-loader

---
+++ webpack.parts.js
@@ -0,0 +1,24 @@
+const { WebpackPluginServe } = require("webpack-plugin-serve");
+const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
+
+exports.devServer = () => ({
+  watch: true,
+  plugins: [
+    new WebpackPluginServe({
+      port: parseInt(process.env.PORT, 10) || 8080,
+      static: "./dist", // Expose if output.path changes
+      liveReload: true,
+      waitForBuild: true,
+    }),
+  ],
+});
+
+exports.page = ({ title }) => ({
+  plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
+});
+
+exports.loadCSS = () => ({
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+});
