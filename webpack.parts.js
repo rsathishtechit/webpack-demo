8fe9c205bf336a9f958b9028f4409ddab182f1bd
Sathishkumar R <rsathishtechit@gmail.com>
2023-08-10 14:58:29 +0530

    Added CSS extract plugin

--- webpack.parts.js
+++ webpack.parts.js
@@ -1,5 +1,6 @@
 const { WebpackPluginServe } = require("webpack-plugin-serve");
 const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 exports.devServer = () => ({
   watch: true,
@@ -17,8 +18,24 @@ exports.page = ({ title }) => ({
   plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
 });
 
-exports.loadCSS = () => ({
-  module: {
-    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
-  },
-});
+exports.extractCSS = ({ options = {}, loaders = [] } = {}) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader, options },
+            "css-loader",
+          ].concat(loaders),
+          sideEffects: true,
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+    ],
+  };
+};
